bdb169194da0e09b0a47a676a427deadf04b19ba
Chauncey McAskill <chauncey@locomotive.ca>
2020-10-29 14:14:09 -0400

    Add disable-post-tag module

---
+++ src/Modules/DisablePostTag/Module.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ecocide\Modules\DisablePostTag;
+
+/**
+ * Disable WordPress Post Tags
+ */
+class Module implements \Ecocide\Contracts\Modules\Module
+{
+    const EVENT_PREFIX = 'ecocide/modules/disable_post_tag/';
+
+    /**
+     * A reference to an instance of this class.
+     *
+     * @var static
+     */
+    private static $instance;
+
+    /**
+     * Boots the module.
+     *
+     * @access public
+     * @param  array $args {
+     *     An array of optional arguments to customize the module.
+     *
+     *     @type array $hooks TODO: Define customizable hooks.
+     * }
+     * @return void
+     */
+    public function boot( array $args = [] ) : void
+    {
+        add_filter( 'post_tag_rewrite_rules', '__return_empty_array', 50 );
+
+        add_filter( 'register_taxonomy_args', [ $this, 'register_taxonomy_args' ], 50, 2 );
+    }
+
+    /**
+     * Disables public access, querying, and UI to the 'post_tag' taxonomy.
+     *
+     * @listens WP#filter:register_taxonomy_args
+     *
+     * @param  array  $args      Array of arguments for registering a taxonomy.
+     * @param  string $post_type Taxonomy key.
+     * @return array  $args
+     */
+    public function register_taxonomy_args( array $args, string $taxonomy ) : array
+    {
+        if ( 'post_tag' === $taxonomy ) {
+            $args['rewrite']           = false;
+            $args['public']            = false;
+            $args['show_ui']           = false;
+            $args['show_admin_column'] = false;
+        }
+
+        return $args;
+    }
+
+    /**
+     * Returns the instance of the module.
+     *
+     * @access public
+     * @return static
+     */
+    public static function get_instance()
+    {
+        // If the single instance hasn't been set, set it now.
+        if ( null === static::$instance ) {
+            static::$instance = new static;
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     * Calls the requested method from the module.
+     *
+     * @param  string  $method The method to ne called.
+     * @param  array   $args   Zero or more parameters to be passed to the method.
+     * @return mixed
+     */
+    public static function __callStatic( $method, $args )
+    {
+        $instance = static::get_instance();
+
+        return $instance ? $instance->$method( ...$args ) : null;
+    }
+}
